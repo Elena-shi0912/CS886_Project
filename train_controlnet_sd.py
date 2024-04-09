edcd61bb8ad1e43ff25f9795081ca451e353daf9
Elena-shi0912 <shiyunyi2002@outlook.com>
2024-04-09 01:04:36 -0400

    update train code

--- train_controlnet_sd.py
+++ train_controlnet_sd.py
@@ -318,10 +318,10 @@ def main(
     params_to_optimize = controlnet.parameters()
     optimizer = torch.optim.AdamW(
         params_to_optimize,
-        lr=args.learning_rate,
-        betas=(args.adam_beta1, args.adam_beta2),
-        weight_decay=args.adam_weight_decay,
-        eps=args.adam_epsilon,
+        lr=train_args.learning_rate,
+        betas=(train_args.adam_beta1, train_args.adam_beta2),
+        weight_decay=train_args.adam_weight_decay,
+        eps=train_args.adam_epsilon,
     )
 
         
@@ -550,7 +550,7 @@ def main(
     max_train_steps = train_args.num_epochs * num_update_steps_per_epoch
 
     lr_scheduler = get_scheduler(
-        args.lr_scheduler,
+        train_args.lr_scheduler,
         optimizer=optimizer,
         num_warmup_steps=train_args.lr_warmup_steps * accelerator.num_processes,
         num_training_steps=max_train_steps * accelerator.num_processes,
@@ -582,7 +582,7 @@ def main(
 
 
     # Train!
-    total_batch_size = args.batch_size * accelerator.num_processes * args.gradient_accumulation_steps
+    total_batch_size = train_args.batch_size * accelerator.num_processes * train_args.gradient_accumulation_steps
 
     logger.info("***** Running training *****")
     logger.info(f"  Num examples = {len(train_dataset)}")
@@ -705,18 +705,18 @@ def main(
                 
                 ce_loss = ce_criterion(segmenter(pred_x0.float()), resized_charmap.squeeze(1))
                 mse_loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean") 
-                loss = mse_loss + ce_loss * args.character_aware_loss_lambda 
+                loss = mse_loss + ce_loss * train_args.character_aware_loss_lambda 
                 
-                avg_loss = accelerator.gather(loss.repeat(args.train_batch_size)).mean()
-                train_loss += avg_loss.item() / args.gradient_accumulation_steps
+                avg_loss = accelerator.gather(loss.repeat(train_args.train_batch_size)).mean()
+                train_loss += avg_loss.item() / train_args.gradient_accumulation_steps
 
                 accelerator.backward(loss)
                 if accelerator.sync_gradients:
                     params_to_clip = controlnet.parameters()
-                    accelerator.clip_grad_norm_(params_to_clip, args.max_grad_norm)
+                    accelerator.clip_grad_norm_(params_to_clip, train_args.max_grad_norm)
                 optimizer.step()
                 lr_scheduler.step()
-                optimizer.zero_grad(set_to_none=args.set_grads_to_none)
+                optimizer.zero_grad(set_to_none=train_args.set_grads_to_none)
 
             # Checks if the accelerator has performed an optimization step behind the scenes
             if accelerator.sync_gradients:
@@ -724,16 +724,16 @@ def main(
                 global_step += 1
 
                 if accelerator.is_main_process:
-                    if global_step % args.checkpointing_steps == 0:
+                    if global_step % train_args.checkpointing_steps == 0:
                         # _before_ saving state, check if this save would set us over the `checkpoints_total_limit`
-                        if args.checkpoints_total_limit is not None:
-                            checkpoints = os.listdir(args.output_dir)
+                        if train_args.checkpoints_total_limit is not None:
+                            checkpoints = os.listdir(model_args.output_dir)
                             checkpoints = [d for d in checkpoints if d.startswith("checkpoint")]
                             checkpoints = sorted(checkpoints, key=lambda x: int(x.split("-")[1]))
 
                             # before we save the new checkpoint, we need to have at _most_ `checkpoints_total_limit - 1` checkpoints
-                            if len(checkpoints) >= args.checkpoints_total_limit:
-                                num_to_remove = len(checkpoints) - args.checkpoints_total_limit + 1
+                            if len(checkpoints) >= train_args.checkpoints_total_limit:
+                                num_to_remove = len(checkpoints) - train_args.checkpoints_total_limit + 1
                                 removing_checkpoints = checkpoints[0:num_to_remove]
 
                                 logger.info(
@@ -742,10 +742,10 @@ def main(
                                 logger.info(f"removing checkpoints: {', '.join(removing_checkpoints)}")
 
                                 for removing_checkpoint in removing_checkpoints:
-                                    removing_checkpoint = os.path.join(args.output_dir, removing_checkpoint)
+                                    removing_checkpoint = os.path.join(model_args.output_dir, removing_checkpoint)
                                     shutil.rmtree(removing_checkpoint)
 
-                        save_path = os.path.join(args.output_dir, f"checkpoint-{global_step}")
+                        save_path = os.path.join(model_args.output_dir, f"checkpoint-{global_step}")
                         accelerator.save_state(save_path)
                         logger.info(f"Saved state to {save_path}")
 
@@ -766,14 +766,14 @@ def main(
             progress_bar.set_postfix(**logs)
             accelerator.log(logs, step=global_step)
 
-            if global_step >= args.max_train_steps:
+            if global_step >= train_args.max_train_steps:
                 break
 
     # Create the pipeline using using the trained modules and save it.
     accelerator.wait_for_everyone()
     if accelerator.is_main_process:
         controlnet = unwrap_model(controlnet)
-        controlnet.save_pretrained(args.output_dir)
+        controlnet.save_pretrained(model_args.output_dir)
 
         
 
